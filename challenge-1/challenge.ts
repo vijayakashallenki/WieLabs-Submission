ddaa24933179b0a9d96c6b4bccf7028e9a20bb41
vijayakash <83111657+vijayakashallenki@users.noreply.github.com>
2023-10-16 13:36:03 +0530

    Submission

--- challenge-1/challenge.ts
+++ challenge-1/challenge.ts
@@ -9,12 +9,22 @@ import { deCompressData } from './decompresshelper';
 import { databaseStr } from "./databasestrhelper";
 export async function processDataDump() {
 //helper function for downloading the data from https://wielabs-task.s3.ap-south-1.amazonaws.com/dump.tar.gz 
-   // await downloadData();
-
+   try{
+    await downloadData();
+   }catch(err){
+      console.log(err);
+   }
 //helper function for decompressing the data to CSV file
+try{
    await deCompressData();
- 
+}catch(err){
+   console.log(err);
+}
 //helper function for storing the csv files in the table at sqlite3 database   
+   try{
    await databaseStr();
+   }
+catch(err){
+   console.log(err);
 }
 processDataDump();
